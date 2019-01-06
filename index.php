3d2eb04a73c06420492ddc36040859a18effb573
prawesh11 <praweshlamsalk@gmail.com>
2019-01-06 15:25:18 +0545

    Initial Commit-Codelogic

---
+++ index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Himalayan Co-operative</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="manifest" href="site.webmanifest">
+        <link rel="apple-touch-icon" href="icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="assets/css/aos.css">
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/normalize.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body>
+        <p>Hello world! This is HTML5 Boilerplate.</p>
+
+        <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+        <script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+        <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="assets/js/aos.js"></script>
+        <script type="text/javascript" src="assets/js/script.js"></script>
+        <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+    </body>
+</html>
