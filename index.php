393351d393e6dcfbebba64659e76095ea8d7f1c9
prawesh11 <praweshlamsalk@gmail.com>
2019-01-06 17:56:17 +0545

    version 2 codelogic

--- index.php
+++ index.php
@@ -6,23 +6,64 @@
         <title>Himalayan Co-operative</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <link rel="manifest" href="site.webmanifest">
         <link rel="apple-touch-icon" href="icon.png">
         <!-- Place favicon.ico in the root directory -->
-
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="assets/css/aos.css">
         <link rel="stylesheet" href="assets/css/bootstrap.min.css">
         <link rel="stylesheet" href="assets/css/normalize.css">
+        <link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
+        <link rel ="stylesheet" type="text/css" href="slick/slick.css">
+        <link rel ="stylesheet" type="text/css" href="slick/slick-theme.css">
         <link rel="stylesheet" href="assets/css/style.css">
     </head>
     <body>
-        <p>Hello world! This is HTML5 Boilerplate.</p>
+        <section id="navigation-bar">
+            <nav class="navbar navbar-default">
+                  <div class="container">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                      </button>
+                      <a class="navbar-brand" href="#">
+                          <img class="img img-responsive" src="assets/images/final_logo.png">
+                      </a>
+                    </div>
 
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                      <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+                        <li><a href="#">Link</a></li>
+                      </ul>
+                      <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">Home</a></li>
+                        <li class="active"><a href="#">Activity <span class="sr-only">(current)</span></a></li>
+                        <li><a href="#">About Us</a></li>
+                        <li class="dropdown">
+                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Our Progress <span class="caret"></span></a>
+                          <ul class="dropdown-menu">
+                            <li><a href="#">Action</a></li>
+                            <li><a href="#">Another action</a></li>
+                            <li><a href="#">Something else here</a></li>
+                            <li role="separator" class="divider"></li>
+                            <li><a href="#">Separated link</a></li>
+                          </ul>
+                        </li>
+                      </ul>
+                    </div><!-- /.navbar-collapse -->
+                  </div><!-- /.container-fluid -->
+                </nav>
+        </section>
         <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
         <script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
         <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="slick/slick.min.js"></script>
         <script type="text/javascript" src="assets/js/aos.js"></script>
         <script type="text/javascript" src="assets/js/script.js"></script>
         <script src="https://www.google-analytics.com/analytics.js" async defer></script>
